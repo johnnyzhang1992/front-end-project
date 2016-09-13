994f32ac9603057943f348b5488f6533362a0055
johnnyzhang <1113638674@qq.com>
2016-09-13 23:23:35 +0800

    关键字提示输入

--- Travel Map/app/scripts/controllers/main.js
+++ Travel Map/app/scripts/controllers/main.js
@@ -25,7 +25,7 @@ angular.module('baidumapApp')
                 $('#current_lng').html("经度："+e.point.lng);
             });
         };
-        $scope.box_show = true;
+        $scope.box_show = false;
         $scope.tool_toogle_box = function () {
             $scope.box_show = !$scope.box_show;
         };
@@ -62,7 +62,54 @@ angular.module('baidumapApp')
         };
         $scope.route_loading = function () {
             
+        };
+        // 百度地图API功能
+        function G(id) {
+            return document.getElementById(id);
         }
+
+        var ac = new BMap.Autocomplete({//建立一个自动完成的对象
+                "input" : "sole-input"
+                ,"location" : bdMapController.map
+            });
+
+        ac.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+            var str = "";
+            var _value = e.fromitem.value;
+            var value = "";
+            if (e.fromitem.index > -1) {
+                value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+            }
+            str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+
+            value = "";
+            if (e.toitem.index > -1) {
+                _value = e.toitem.value;
+                value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+            }
+            str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+            G("searchResultPanel").innerHTML = str;
+        });
+        function setPlace(){
+            bdMapController.map.clearOverlays();    //清除地图上所有覆盖物
+            function myFun(){
+                var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+                bdMapController.map.centerAndZoom(pp, 18);
+                bdMapController.map.addOverlay(new BMap.Marker(pp));    //添加标注
+            }
+            var local = new BMap.LocalSearch(bdMapController.map, { //智能搜索
+                onSearchComplete: myFun
+            });
+            local.search(myValue);
+        }
+        var myValue;
+        ac.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+            var _value = e.item.value;
+            myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+            G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+
+            setPlace();
+        });
     });
 
 
